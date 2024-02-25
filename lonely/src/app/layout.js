e9b5b5a5348966a80e22866edff44f0f22c22b7e
Rishu jha <jharishu796@gmail.com>
2024-02-25 12:19:02 +0000

    rough navbar created

--- lonely/src/app/layout.js
+++ lonely/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ReduxWrapper from "@/redux/ReduxWrapper";
+import NavBar from "@/components/navbar/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <ReduxWrapper>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <NavBar />
+          {children}
+        </body>
       </ReduxWrapper>
     </html>
   );
---
+++ lonely/src/components/navbar/NavBar.js
@@ -0,0 +1,26 @@
+import { LANGUAGES } from "@/constants/LangeConst";
+
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between">
+      <div className="flex justify-between " style={{ width: "70%" }}>
+        <p>Home</p>
+        <p>About</p>
+        <p>contact</p>
+      </div>
+      <div>
+        <select className="rounded p-1">
+          {LANGUAGES.map(({ code, label }) => {
+            return (
+              <option key={code} value={code}>
+                {label}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
