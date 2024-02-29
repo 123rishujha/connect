fe3599af28441b3e51ead953f24f3035c0715913
Rishu jha <jharishu796@gmail.com>
2024-02-29 18:47:23 +0000

    next-intl initial setup

---
+++ lonely/src/app/[locale]/page.js
@@ -0,0 +1,13 @@
+// "use client";
+
+import { useTranslations } from "next-intl";
+
+export default function Home() {
+  // const t = useTranslations("Index");
+  // console.log("909", t);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      locale home page
+    </main>
+  );
+}
