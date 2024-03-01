e8ddbb4e4683afb428e913af36a53b326e3aed27
Rishu jha <jharishu796@gmail.com>
2024-03-01 18:09:20 +0000

    next-intl integrated properly

--- lonely/src/app/[locale]/page.js
+++ lonely/src/app/[locale]/page.js
@@ -1,13 +1,12 @@
 // "use client";
-
 import { useTranslations } from "next-intl";
 
 export default function Home() {
-  // const t = useTranslations("Index");
-  // console.log("909", t);
+  const t = useTranslations("Index");
+  console.log("909", t("title"));
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      locale home page
+      {t("title")}
     </main>
   );
 }
--- lonely/src/i18n.js
+++ lonely/src/i18n.js
@@ -1,13 +1,14 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 
+// Can be imported from a shared config
 const locales = ["en", "hi"];
 
 export default getRequestConfig(async ({ locale }) => {
+  // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale)) notFound();
 
   return {
-    // messages: (await import(`../messages/${locale}.js.on`)).default,
-    messages: (await import(`../messages/en.json`)).default,
+    messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
