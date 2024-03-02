59759bcd8f52fbf422a5a999bd6e10d2698123cb
Rishu jha <jharishu796@gmail.com>
2024-03-02 18:22:42 +0000

    next-intl setup created

--- lonely/src/app/[locale]/page.js
+++ lonely/src/app/[locale]/page.js
@@ -2,11 +2,11 @@
 import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations("Index");
-  console.log("909", t("title"));
+  const t = useTranslations("IndexPage");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {t("title")}
+      <h1>{t("title")}</h1>
+      <p>{t("description")}</p>
     </main>
   );
 }
